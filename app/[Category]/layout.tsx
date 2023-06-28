a579b06e240e8f05fd5b832ff41d14c746dc8d24
josandg <gussanjoba@student.gu.se>
2023-06-28 14:00:27 +0200

    suspenses on cards testing

--- app/[Category]/layout.tsx
+++ app/[Category]/layout.tsx
@@ -7,6 +7,7 @@ import TextTruncate from '@/components/CategoryDescription';
 import { client } from '@/sanity/lib/client';
 import { getCategoryByParams } from '@/sanity/helpers/queries';
 import CategoryNavbar from '@/components/CategoryNavbar';
+import { Suspense } from 'react';
 
 async function getCategory(params: string) {
   const res = await client.fetch(getCategoryByParams(params));
@@ -32,16 +33,18 @@ export default async function CategoryLayout({
   return (
     <section className='flex text-black gap-5 bg-gray-300 pt-1'>
       {/* Include shared UI here e.g. a header or sidebar */}
-      <nav className='hidden lg:flex flex-[3] gap-3 flex-col '>
-        <IconBreadcrumbs params={params.Category} />
-        <span className=' '>
-          {' '}
-          <h1 className='text-xl font-semibold'>{categoryData.title}</h1>{' '}
-          <TextTruncate text={categoryData?.description} maxLines={2} />
-        </span>
+      <Suspense fallback={<div className='bg-red-600'>Loading nav</div>}>
+        <nav className='hidden lg:flex flex-[3] gap-3 flex-col '>
+          <IconBreadcrumbs params={params.Category} />
+          <span className=' '>
+            {' '}
+            <h1 className='text-xl font-semibold'>{categoryData.title}</h1>{' '}
+            <TextTruncate text={categoryData?.description} maxLines={2} />
+          </span>
 
-        <CategoryNavbar currentCateogry={params.Category} />
-      </nav>
+          <CategoryNavbar currentCateogry={params.Category} />
+        </nav>
+      </Suspense>
 
       <main className='flex-[8] w-full '>{children}</main>
     </section>
--- app/[Category]/page.tsx
+++ app/[Category]/page.tsx
@@ -1,7 +1,7 @@
 import RatingComponent from '@/components/RatingComponent';
 import { getProductsByCategory } from '@/sanity/helpers/queries';
 import { client } from '@/sanity/lib/client';
-import React from 'react';
+import React, { Suspense } from 'react';
 
 async function getProducts(categoryHref: string) {
   const data = await client.fetch(getProductsByCategory(categoryHref));
@@ -44,43 +44,48 @@ export default async function page({
         // we only have 1 image per product for now, it's too much work to add more
         if (!product.Images) return null;
         return (
-          <div
+          <Suspense
             key={product.ArtikelNummer}
-            className='flex border bg-white justify-between gap-2 flex-col p-3'
+            fallback={<div className='bg-red-600'>Loading cards</div>}
           >
-            <img
-              className='cursor-pointer max-w-full h-auto'
-              src={product.Images[0].asset.url}
-              alt=''
-            />
-            <div>
-              <h2 className='font-bold text-md'>{product.CardName}</h2>
-              <p className='text-gray-600 font-light text-xs'>
-                {product.title}
-              </p>
-            </div>
-            <RatingComponent />
+            <div
+              key={product.ArtikelNummer}
+              className='flex border bg-white justify-between gap-2 flex-col p-3'
+            >
+              <img
+                className='cursor-pointer max-w-full h-auto'
+                src={product.Images[0].asset.url}
+                alt=''
+              />
+              <div>
+                <h2 className='font-bold text-md'>{product.CardName}</h2>
+                <p className='text-gray-600 font-light text-xs'>
+                  {product.title}
+                </p>
+              </div>
+              <RatingComponent />
 
-            <div className='flex flex-col justify-between'>
-              <ul className='list-disc text-sm text-gray-600 list-inside'>
-                {product.bulletPoints.map((bulletPoint) => (
-                  <li key={bulletPoint}>{bulletPoint}</li>
-                ))}
-              </ul>
+              <div className='flex flex-col justify-between'>
+                <ul className='list-disc text-sm text-gray-600 list-inside'>
+                  {product.bulletPoints.map((bulletPoint) => (
+                    <li key={bulletPoint}>{bulletPoint}</li>
+                  ))}
+                </ul>
 
-              <div className=''>
-                <span className='text-red-700 font-semibold text-3xl'>
-                  {product.details.price.toLocaleString('sv-SE')}:-
-                </span>
+                <div className=''>
+                  <span className='text-red-700 font-semibold text-3xl'>
+                    {product.details.price.toLocaleString('sv-SE')}:-
+                  </span>
 
-                <span className='flex justify-center w-full  items-center'>
-                  <button className='bg-light-blue-600 hover:bg-light-blue-900 transition-colors w-full text-white font-semibold py-2 px-4 border-blue-700 rounded'>
-                    Lägg i varukorg
-                  </button>
-                </span>
+                  <span className='flex justify-center w-full  items-center'>
+                    <button className='bg-light-blue-600 hover:bg-light-blue-900 transition-colors w-full text-white font-semibold py-2 px-4 border-blue-700 rounded'>
+                      Lägg i varukorg
+                    </button>
+                  </span>
+                </div>
               </div>
             </div>
-          </div>
+          </Suspense>
         );
       })}
     </div>
