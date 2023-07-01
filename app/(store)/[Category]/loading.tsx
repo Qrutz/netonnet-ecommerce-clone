4a0d39d7fc5223c370ef350a913fd9e77240bade
josandg <gussanjoba@student.gu.se>
2023-07-01 15:05:11 +0200

    skeleton update, removed suspense because we dont need it since the entire page is async

--- app/(store)/[Category]/loading.tsx
+++ app/(store)/[Category]/loading.tsx
@@ -1,3 +1,6 @@
+import SkeletonComponent from '@/components/ProductSkeleton';
+import Skeleton from '@/utils/LibraryCompsToClientConvert/Skeleton';
+
 export default function Loading() {
-  return <div> Loading the page? </div>;
+  return <SkeletonComponent />;
 }
